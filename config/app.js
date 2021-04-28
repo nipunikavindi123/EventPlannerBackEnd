adb025e5b273ce83c7499bc9d4d9fbef77ac4b9d
nipuni <nipunikavindi1507@gmail.com>
2021-04-28 18:35:57 +0530

    base url

--- config/app.js
+++ config/app.js
@@ -5,7 +5,7 @@ if(process.env.APP_ENVIRONMENT === 'production'){
     module.exports = {
         app: {
             environment : "production",
-            base_url    :  "http://localhost:3030/",
+            base_url    :  "https://boiling-wave-30400.herokuapp.com/",
             fileSizeLimit : 10485760,
             allowedMimeType : ['image/jpeg', 'image/png', 'image/gif'],
             database_uri : "mongodb+srv://admin:admin@eventplanner.3n4uf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
@@ -15,7 +15,7 @@ if(process.env.APP_ENVIRONMENT === 'production'){
     module.exports = {
         app: {
             environment : "development",
-            base_url    :  "http://localhost:3030/",
+            base_url    :  "https://boiling-wave-30400.herokuapp.com/",
             fileSizeLimit : 10485760,
             allowedMimeType : ['image/jpeg', 'image/png', 'image/gif'],
             database_uri : "mongodb+srv://admin:admin@eventplanner.3n4uf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",  
